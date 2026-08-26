105649462461f7aa1dcd0fb53cee87fc074626a9
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-26 16:20:09 +0800

    Balance Better Scanner range and restore default starting money (v3.8.6)

--- lethal_company_patcher.py
+++ lethal_company_patcher.py
@@ -21,7 +21,7 @@ import urllib.error
 REPO_USER = "hayagerwin"
 REPO_NAME = "lc-mods-patcher"
 BRANCH = "main"
-PATCHER_VERSION = "20260826160357"
+PATCHER_VERSION = "20260826162003"
 
 # ==============================================================================
 # TERMINAL FORMATTING HELPERS
