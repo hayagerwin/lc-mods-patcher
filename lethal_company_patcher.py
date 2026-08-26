b201ceafb59c65ab4002bd5ebbff785525b4b678
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-26 22:01:54 +0800

    Restore Line of Sight requirements for exits in Better Scanner (v3.9.5)

--- lethal_company_patcher.py
+++ lethal_company_patcher.py
@@ -21,7 +21,7 @@ import urllib.error
 REPO_USER = "hayagerwin"
 REPO_NAME = "lc-mods-patcher"
 BRANCH = "main"
-PATCHER_VERSION = "20260826201755"
+PATCHER_VERSION = "20260826220151"
 
 # ==============================================================================
 # TERMINAL FORMATTING HELPERS
