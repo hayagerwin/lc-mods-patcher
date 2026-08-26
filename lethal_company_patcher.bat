8a7b594d78ab50b8c7857fabea5f07b39209db11
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-26 16:45:01 +0800

    Eliminate HarmonyX AccessTools warnings and clean up Better Scanner reflection (v3.8.7)

--- lethal_company_patcher.bat
+++ lethal_company_patcher.bat
@@ -9,7 +9,7 @@ REM ============================================================================
 set "REPO_USER=hayagerwin"
 set "REPO_NAME=lc-mods-patcher"
 set "BRANCH=main"
-set "PATCHER_VERSION=20260826162003"
+set "PATCHER_VERSION=20260826164458"
 
 REM Script directory and config path
 set "SCRIPT_DIR=%~dp0"
