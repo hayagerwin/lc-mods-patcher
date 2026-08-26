18e0a3b5ab861e029ae78963010bcabf3357cf57
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-27 05:32:29 +0800

    Implement Dynamic Weather & Moon Risk Scrap Scaling in ScrapSyncFix (v3.9.6)

--- lethal_company_patcher.py
+++ lethal_company_patcher.py
@@ -21,7 +21,7 @@ import urllib.error
 REPO_USER = "hayagerwin"
 REPO_NAME = "lc-mods-patcher"
 BRANCH = "main"
-PATCHER_VERSION = "20260826220151"
+PATCHER_VERSION = "20260827053226"
 
 # ==============================================================================
 # TERMINAL FORMATTING HELPERS
