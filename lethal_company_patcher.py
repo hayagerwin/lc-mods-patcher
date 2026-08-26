f6fc1dd609ce3d0f6f302ebff0118b788498e37d
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-26 14:53:19 +0800

    Add Authoritative Moon Scrap Synchronization (v3.7.8)

--- lethal_company_patcher.py
+++ lethal_company_patcher.py
@@ -21,7 +21,7 @@ import urllib.error
 REPO_USER = "hayagerwin"
 REPO_NAME = "lc-mods-patcher"
 BRANCH = "main"
-PATCHER_VERSION = "20260826144257"
+PATCHER_VERSION = "20260826145317"
 
 # ==============================================================================
 # TERMINAL FORMATTING HELPERS
