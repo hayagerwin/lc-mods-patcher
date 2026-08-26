706bc14808bfd00dd9f1a7e21d6ef5c3c247f008
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-26 16:04:01 +0800

    Fix scanner nodesOnScreen clear buffer and disable GI personal scanner (v3.8.5)

--- lethal_company_patcher.py
+++ lethal_company_patcher.py
@@ -21,7 +21,7 @@ import urllib.error
 REPO_USER = "hayagerwin"
 REPO_NAME = "lc-mods-patcher"
 BRANCH = "main"
-PATCHER_VERSION = "20260826155302"
+PATCHER_VERSION = "20260826160357"
 
 # ==============================================================================
 # TERMINAL FORMATTING HELPERS
