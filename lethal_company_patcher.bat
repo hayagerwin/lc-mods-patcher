625b2cb664336a1e0c56876638d30b40d43151f0
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-27 07:52:14 +0800

    Fix VoIP microphone dropouts and ModelReplacement render exceptions (v3.9.9)

--- lethal_company_patcher.bat
+++ lethal_company_patcher.bat
@@ -9,7 +9,7 @@ REM ============================================================================
 set "REPO_USER=hayagerwin"
 set "REPO_NAME=lc-mods-patcher"
 set "BRANCH=main"
-set "PATCHER_VERSION=20260827064248"
+set "PATCHER_VERSION=20260827075211"
 
 REM Script directory and config path
 set "SCRIPT_DIR=%~dp0"
