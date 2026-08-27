8609aced8e4813da367796cb925817e5b81258b9
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-27 18:43:53 +0800

    Phase 3 Fidelity-First Ship Lobby & Camera Optimization (v4.8.0)

--- lethal_company_patcher.py
+++ lethal_company_patcher.py
@@ -21,7 +21,7 @@ import urllib.error
 REPO_USER = "hayagerwin"
 REPO_NAME = "lc-mods-patcher"
 BRANCH = "main"
-PATCHER_VERSION = "20260827181631"
+PATCHER_VERSION = "20260827184348"
 
 # ==============================================================================
 # TERMINAL FORMATTING HELPERS
