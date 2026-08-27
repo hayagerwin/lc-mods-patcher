39affee6183889795e84d22df6681a245271beef
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-27 19:11:07 +0800

    Zero daytime enemy power for undisturbed free roam on 32 Argent (v4.8.2)

--- lethal_company_patcher.py
+++ lethal_company_patcher.py
@@ -21,7 +21,7 @@ import urllib.error
 REPO_USER = "hayagerwin"
 REPO_NAME = "lc-mods-patcher"
 BRANCH = "main"
-PATCHER_VERSION = "20260827190545"
+PATCHER_VERSION = "20260827191100"
 
 # ==============================================================================
 # TERMINAL FORMATTING HELPERS
