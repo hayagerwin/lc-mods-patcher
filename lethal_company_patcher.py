893c67e6aaf221c4ab6e8b5e345906653d63f8dd
Erwin Hayag <hayagerwin85@gmail.com>
2026-08-27 17:32:50 +0800

    Prevent OnlineFix URL browser pop-up on launch

--- lethal_company_patcher.py
+++ lethal_company_patcher.py
@@ -21,7 +21,7 @@ import urllib.error
 REPO_USER = "hayagerwin"
 REPO_NAME = "lc-mods-patcher"
 BRANCH = "main"
-PATCHER_VERSION = "20260827172312"
+PATCHER_VERSION = "20260827173241"
 
 # ==============================================================================
 # TERMINAL FORMATTING HELPERS
